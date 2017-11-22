2ce643e633dd131f824e4709fa8dd73c96aa422c
cluckj <cluckj@users.noreply.github.com>
2017-11-22 13:04:45 -0500

    Update getip.sh

--- scripts/getip.sh
+++ scripts/getip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-IP=$(ip -f inet -o addr show wlan0|cut -d\  -f 7 | cut -d/ -f 1)
-echo -e "Current IP Address:\n$IP\n"
-echo -e "To connect: ssh\npi@$IP\n"
+WIFI_IP=$(ip -f inet -o addr show wlan0|cut -d\  -f 7 | cut -d/ -f 1)
+BT_IP=$(ip -f inet -o addr show bnep0|cut -d\  -f 7 | cut -d/ -f 1)
+echo -e "Current IP Address:\n$WIFI_IP\n"
+echo -e "Current BT IP:\n$BT_IP\n"
