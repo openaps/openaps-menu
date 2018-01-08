be55b1183c8d3e315ebfc37f6c6990092b74d5b0
cluckj <cluckj@gmail.com>
2018-01-07 23:25:17 -0500

    Revert "Menu personalization updates"
    This reverts commit 72cfff537694647331741f3bee5c2cc9ada998fe.

--- scripts/getip.sh
+++ scripts/getip.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-WIFI_IP=$(ip -f inet -o addr show wlan0|cut -d\  -f 7 | cut -d/ -f 1)
-BT_IP=$(ip -f inet -o addr show bnep0|cut -d\  -f 7 | cut -d/ -f 1)
-echo -e "\nCurrent WiFi IP:\n$WIFI_IP\n"
-echo -e "\nCurrent BT IP:\n$BT_IP\n"
+IP=$(ip -f inet -o addr show wlan0|cut -d\  -f 7 | cut -d/ -f 1)
+echo -e "Current IP Address:\n$IP\n"
+echo -e "To connect: ssh\npi@$IP\n"
