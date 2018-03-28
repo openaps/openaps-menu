0b928bd705d4cc813bd11b0b757af4959f45f5e5
Jon Cluck <cluckj@users.noreply.github.com>
2018-03-28 19:28:08 -0400

    Time mapping (#11)
    * Status screen
    
    Really don't start using this yet, it doesn't do everything it needs to...
    
    * More data!!!
    
    Add timeago, IOB, and COB; make bg graph nicer looking (shows slightly over 3 hours of data now).
    
    * Delete status.js
    
    * Time-based rendering
    
    Will render x value on the graph based on time, rather than number of BGs.
    
    * Wrong y calculation
    
    * alignment of lines
    
    * graph boundaries

--- scripts/status.js
+++ scripts/status.js
@@ -91,7 +91,7 @@ if(batterylevel) {
 
 //Create and render clock
 function displayClock() {
-  var date=new Date();
+  var date = new Date();
   var hour = date.getHours();
   hour = (hour < 10 ? "0" : "") + hour;
   var min  = date.getMinutes();
@@ -110,11 +110,16 @@ display.oled.drawLine(2, 30, 5, 30, 1);
 display.oled.drawLine(2, 40, 5, 40, 1);
 
 //render BG graph
-var i = (suggested.predBGs != undefined) ? (72) : (120); //fill the whole graph with BGs if there are no predictions
-var x = 5; //start in the right place
-for (i; i >= 0; i--) {
-    x = x + 1;
+var numBGs = (suggested.predBGs != undefined) ? (72) : (120); //fill the whole graph with BGs if there are no predictions
+var date = new Date();
+var zerotime = date.getTime() - ((numBGs * 5) * 600);
+var zero_x = numBGs + 5;
+for (var i = 0; i <= numBGs; i++) {
+    var x = 2 + zero_x + Math.round(((((bg[i].date - zerotime)/1000)/60)/5));
     var y = Math.round( 21 - ( ( bg[i].glucose - 250 ) / 8 ) );
+    //left and right boundaries
+    if ( x < 5 ) x = 5;
+    if ( x > 127 ) x = 127;
     //upper and lower boundaries
     if ( y < 21 ) y = 21;
     if ( y > 51 ) y = 51;
@@ -124,15 +129,16 @@ for (i; i >= 0; i--) {
 //render predictions, only if we have them
 if (suggested.predBGs != undefined) {
   //render line between actual BG and predicted
-  x = x + 1;
+  x = zero_x + 3;
   display.oled.drawLine(x, 51, x, 21, 1);
   //render predictions
   var predictions = [suggested.predBGs.IOB, suggested.predBGs.ZT, suggested.predBGs.UAM, suggested.predBGs.COB];
-  x = x - 2;
   for (i = 0; i <= 48; i++) {
-      x = x + 1
+      x++;
       for(var n = 0; n <=3 && (predictions[n] != undefined); n++) {
       y = Math.round( 21 - ( (predictions[n][i] - 250 ) / 8) );
+      //right boundary
+      if ( x > 127 ) x = 127;
       //upper and lower boundaries
       if ( y < 21 ) y = 21;
       if ( y > 51 ) y = 51;
