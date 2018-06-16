3bd0b9d74178487ca9facb7e1a32104b81fed420
Peter <kretep@users.noreply.github.com>
2018-06-16 21:51:59 +0200

    Prevent newline
    to make sure the output is strictly empty when voltage is not available.

--- scripts/getvoltage.sh
+++ scripts/getvoltage.sh
@@ -3,5 +3,5 @@
 command -v socat >/dev/null 2>&1 || { echo >&2 "I require socat but it's not installed. Aborting."; exit 1; }
 
 RESPONSE=`echo '{"command":"read_voltage"}' | socat -,ignoreeof ~/src/openaps-menu/socket-server.sock | sed -n 's/.*"response":\([^}]*\)}/\1/p'`
-echo $RESPONSE
+[[ $RESPONSE = *[![:space:]]* ]] && echo $RESPONSE
 #./getvoltage.sh | sed -n 's/.*"response":\([^}]*\)}/\1/p'
