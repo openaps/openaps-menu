8738aadea090c0515d6dc42b1dfd223e389f1c1e
Dave Carlson <thecubic@thecubic.net>
2018-08-18 16:13:27 -0700

    fix buttons-needs-cwd

--- openaps-menu.sh
+++ openaps-menu.sh
@@ -6,5 +6,7 @@ buttonsdir=${topdir}/lib/pi-buttons
 # GPIO setup and buttons utility build
 ${buttonsdir}/setup.sh
 
-${buttonsdir}/buttons &
+# needs to create a socket in cwd
+(cd ${buttonsdir} && ./buttons &)
+
 exec node ${topdir}/index.js
